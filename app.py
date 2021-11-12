21cb64cdb2937731b521a1acb01dca43e1e84d6b
danielhquez <94133504+danielhquez@users.noreply.github.com>
2021-11-12 16:03:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request, Response
+
 app = Flask(__name__)
 
 
 
 @app.route("/")
-def hello ():
-    return "Hello Carla y Daniel"
+def hello():
+    user = request.args.get('user')
+    password = request.args.get('password')
+    if (user=='atlanticomedio') and(password== '1234' ):
+        return "ok"
+    else:
+        return "ko"
 
 if __name__ == '__main__':
      app.run(port='5000')
